ed9ba42fea270f8f5ad27dfbeb117f5ca8ec7aa1
akmaltezar <86656918+akmaltezar@users.noreply.github.com>
2021-07-28 03:34:08 +0700

    Add files via upload

---
+++ soal1.js
@@ -0,0 +1,6 @@
+var listDesaBantul = [
+    ["Kec.Pandak :" , "Caturharjo","Triharjo", "Gilangharjo", "Wirirejo"],
+    ["Kec.Kretek :" , "Tirtomulyo", "Parangtritis", "Donotirto", "Tirtosari", "Tirtohargo"],
+    ["Kec.Bantul :" , "Ringinharjo", "Palbapang", "Trirenggo", "Sabdodadi", "Bantul"]
+];
+console.log(listDesaBantul.toString());
